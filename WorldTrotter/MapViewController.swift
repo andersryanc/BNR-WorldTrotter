f3b1a71b377252806349a99cf17f03119dd63a25
Ryan Anderson <anderson.ryanc@gmail.com>
2016-12-08 23:23:50 -0800

    add zoom to user location button to map

--- WorldTrotter/MapViewController.swift
+++ WorldTrotter/MapViewController.swift
@@ -8,15 +8,23 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
     var mapView: MKMapView!
+    var zoomButton: UIButton!
+    
+    let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         mapView = MKMapView()
+        mapView.showsUserLocation = true
         view = mapView
-        
+
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+                
         let segmentedControl = UISegmentedControl(items: ["Standard","Hybrid","Satellite"])
         segmentedControl.backgroundColor = UIColor.init(white: 1, alpha: 0.5)
         segmentedControl.selectedSegmentIndex = 0
@@ -33,8 +41,44 @@ class MapViewController: UIViewController {
         trailingConstraint.isActive = true
         
         segmentedControl.addTarget(self, action: #selector(mapTypeChanged(segControl:)), for: .valueChanged)
+        
+        let zoomIcon = UIImage(named: "ZoomIcon")
+        zoomButton = UIButton(type: .custom) as UIButton
+//        zoomButton.setTitle("", for: .normal)
+//        zoomButton.frame = CGRect(x: 100, y: 100, width: 100, height: 100)
+        zoomButton.setImage(zoomIcon, for: .normal)
+        zoomButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(zoomButton)
+        
+        let zbBottomConstraint = zoomButton.bottomAnchor.constraint(equalTo: bottomLayoutGuide.topAnchor, constant: -8)
+        let zbTrailingConstraint = zoomButton.trailingAnchor.constraint(equalTo: margins.trailingAnchor)
+        
+        zbBottomConstraint.isActive = true
+        zbTrailingConstraint.isActive = true
+        
+        zoomButton.addTarget(self, action: #selector(zoomToLocation(button:)), for: .touchUpInside)
     }
+
+//    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+//        switch status {
+//        case .denied:
+//            print("location denied")
+//        case .authorizedWhenInUse:
+//            print("location when in use")
+//        case .notDetermined:
+//            manager.requestWhenInUseAuthorization()
+//            print("location not determined")
+//        default:
+//            break
+//        }
+//    }
     
+//    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+//        locationManager?.stopUpdatingLocation()
+//        locationManager?.store(location: locations, callback: { (stored) in
+//        })
+//    }
+
     func mapTypeChanged(segControl: UISegmentedControl) {
         switch segControl.selectedSegmentIndex {
         case 0:
@@ -49,6 +93,22 @@ class MapViewController: UIViewController {
         default:
             break
         }
+    }
+    
+    func zoomToLocation(button: UIButton) {
+        let location = (mapView.userLocation.location?.coordinate)!
+
+//        let lat: CLLocationDegrees = location.latitude
+//        let lng: CLLocationDegrees = location.longitude
+//        print("\(lat), \(lng)")
+//        mapView.setCenter(location, animated: true)
+
+        let latDelta:CLLocationDegrees = 0.5
+        let lonDelta:CLLocationDegrees = 0.5
+        let span = MKCoordinateSpanMake(latDelta, lonDelta)
+        let region = MKCoordinateRegionMake(location, span)
+
+        mapView.setRegion(region, animated: true)
     }
     
 }
